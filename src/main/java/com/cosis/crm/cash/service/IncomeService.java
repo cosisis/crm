5a1175fe3b7a489e31227396ca26bf9829afdcda
liuj819 <465977818@qq.com>
2018-07-17 15:07:53 +0800

    service

--- src/main/java/com/cosis/crm/cash/service/IncomeService.java
+++ src/main/java/com/cosis/crm/cash/service/IncomeService.java
@@ -1,21 +1,30 @@
 package com.cosis.crm.cash.service;
 
+import com.cosis.crm.cash.controller.IncomeController;
 import com.cosis.crm.cash.domain.Result;
+import com.cosis.crm.cash.domain.ReturnVal;
 import com.cosis.crm.cash.domain.entity.IncomeInfo;
 import com.cosis.crm.cash.enums.RetCodeEnum;
 import com.cosis.crm.cash.mapper.IncomeInfoMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.math.BigDecimal;
+import java.util.Date;
 
 @Service
-public class IncomeService {
+public interface IncomeService {
 
-    @Autowired
-    IncomeInfoMapper infoMapper;
+    /**
+     * 添加理财
+     * @param info
+     * @return
+     */
+    public Result add (IncomeInfo info);
 
-    public Result add (IncomeInfo info){
-        Result result = new Result();
-        result.setRetEnum((infoMapper.add(info) > 0) ? RetCodeEnum.OK : RetCodeEnum.COMMON_ERROR_TRY_AGAIN) ;
-        return result;
-    }
+
 }
